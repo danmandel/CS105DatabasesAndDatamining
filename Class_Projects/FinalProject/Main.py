c0f82f529dff856b3b7a7b83bf66141f6396d18c
danmandel <danmandel@users.noreply.github.com>
2016-04-02 00:17:09 -0400

    Update Main.py

--- Class_Projects/FinalProject/Main.py
+++ Class_Projects/FinalProject/Main.py
@@ -1,16 +1,31 @@
 infile = open('train.csv', 'r')
 outfile = open('train_processed.csv','w')
+infile = "train.csv"
+
+import csv
 
 
-count = 0
-for line in infile:
-    if count == 0:
-        count += 1
-        
-    elif count < 5:
-        components = line.split(",")
-        num = components[0]
-        target = components[1]
-        print ("Target is: {}, num: {}".format(target, count))
-        print ("")
-        count +=1
+
+with open(infile) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',') # Better because \n was messing things up I think.
+    
+    row_counter = 0
+    
+    for row in readCSV:
+        nonblank_fields = 0
+        if row_counter < 10: #eventually can remove this and indent everything under it 1 tab to the left.
+            
+            ID = row[0]
+            Target = row[1]
+            Missing_Fields = []
+
+            for i in row:
+                if (i != ''):
+                    nonblank_fields += 1
+            
+
+            print ("There are {} filled in fields for ID {} \n".format(nonblank_fields,ID))
+          
+            row_counter +=1
+        else:
+            break
