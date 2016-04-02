615077de4d10705ac53d0b431c283036ab8c9baf
danmandel <danmandel@users.noreply.github.com>
2016-04-02 00:48:17 -0400

    Update Main.py

--- Class_Projects/FinalProject/Main.py
+++ Class_Projects/FinalProject/Main.py
@@ -1,16 +1,12 @@
-infile = open('train.csv', 'r')
-outfile = open('train_processed.csv','w')
+#infile = open('train.csv', 'r')
 infile = "train.csv"
+outfile = open('train_processed.csv','w')
 
 import csv
 
-
-
 with open(infile) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',') # Better because \n was messing things up I think.
-    
     row_counter = 0
-    
     for row in readCSV:
         nonblank_fields = 0
         if row_counter < 10: #eventually can remove this and indent everything under it 1 tab to the left.
@@ -18,14 +14,13 @@ with open(infile) as csvfile:
             ID = row[0]
             Target = row[1]
             Missing_Fields = []
-
-            for i in row:
-                if (i != ''):
-                    nonblank_fields += 1
             
+            for field in row:
+                print (row.index(field))
+                if (field != ''):
+                    nonblank_fields += 1
 
-            print ("There are {} filled in fields for ID {} \n".format(nonblank_fields,ID))
-          
+            print ("There are {} filled in fields for ID {} \n".format(nonblank_fields,ID))    
             row_counter +=1
         else:
             break
