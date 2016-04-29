488f3dafbbf1e5b43f289f197e7ce9e1efd81e35
danmandel <danmandel@users.noreply.github.com>
2016-04-28 22:27:17 -0400

    Update and rename AverageFollowers.py to GetAverages.py

--- Class_Projects/FinalProject/AverageFollowers.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('twitterData_train.sqlite')
-
-c = conn.cursor()
-c.execute('''SELECT AVG(A_followers) FROM twitterMeasures WHERE choice = "A"''')
-
-for tuple in c:
-    print(tuple[0])
